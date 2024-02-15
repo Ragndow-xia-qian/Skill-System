d2260cba0e28bfed90e91d83c8705224e26148d3
Ragndow-xia-qian <372378529@qq.com>
2024-02-15 16:42:44 +0800

    Create Files

---
+++ src/main/java/com/XiaQian/skill_system/Item/ModItem.java
@@ -0,0 +1,20 @@
+package com.XiaQian.skill_system.Item;
+
+import com.XiaQian.skill_system.SkillSystem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItem {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, SkillSystem.MODID);
+
+    public static final RegistryObject<Item> SKILL_CARD = ITEMS.register("skill_card",
+            ()->new Item(new Item.Properties().tab(CreativeModeTab.TAB_COMBAT)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
